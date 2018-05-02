b06811330ff57a09cd3733199f78a1ed849cbf4f
Amin Soltani <solamin.amin@gmail.com>
2018-05-01 20:43:32 -0700

    updating compiling commands

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 cls
 flex++.exe .\amin.l
-yacc -d .\amin.y
-g++ .\lex.yy.c .\amin.tab.c -o amin.exe
+yacc -d .\amin.y --file-prefix=amin
+g++ -std=gnu++0x .\lex.yy.c .\amin.tab.c -o amin.exe
 .\amin.exe final.txt
+
